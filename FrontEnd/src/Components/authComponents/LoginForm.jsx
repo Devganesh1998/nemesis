41033d80acd65ece7f605124d32be00f975c0619
devganesh1998 <devganesh91@gmail.com>
2021-04-14 21:56:32 +0530

    email validator added and login page changes

--- FrontEnd/src/Components/authComponents/LoginForm.jsx
+++ FrontEnd/src/Components/authComponents/LoginForm.jsx
@@ -3,78 +3,94 @@ import { Form, Input, Button } from "antd";
 import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
 export const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 10,
-  },
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 10,
+    },
 };
 export const tailLayout = {
-  wrapperCol: {
-    offset: 8,
-    span: 10,
-  },
+    wrapperCol: {
+        offset: 8,
+        span: 10,
+    },
 };
 
 const LoginForm = (props) => {
-  const { loginUser } = props;
-  const onFinish = (values) => {
-    console.log("Success:", values);
-    loginUser(values);
-  };
+    const { loginUser } = props;
+    const onFinish = (values) => {
+        console.log("Success:", values);
+        loginUser(values);
+    };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
+    const onFinishFailed = (errorInfo) => {
+        console.log("Failed:", errorInfo);
+    };
 
-  return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Please input your email!",
-          },
-        ]}
-      >
-        <Input prefix={<MailOutlined style={{ margin: '5px' }} />} />
-      </Form.Item>
+    return (
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your email!",
+                    },
+                    () => ({
+                        validator(_, value) {
+                            if (!value) {
+                                return Promise.resolve();
+                            }
+                            const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                            if (value.match(regexEmail)) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(
+                                new Error("Entered email is not valid!")
+                            );
+                        },
+                    }),
+                ]}
+            >
+                <Input prefix={<MailOutlined style={{ margin: "5px" }} />} />
+            </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-      >
-        <Input.Password prefix={<LockOutlined style={{ margin: "5px" }} />} />
-      </Form.Item>
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: "Please input your password!",
+                    },
+                ]}
+            >
+                <Input.Password
+                    prefix={<LockOutlined style={{ margin: "5px" }} />}
+                />
+            </Form.Item>
 
-      {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            {/* <Form.Item {...tailLayout} name="remember" valuePropName="checked">
         <Checkbox>Remember me</Checkbox>
       </Form.Item> */}
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
                 </Button>
-      </Form.Item>
-    </Form>
-  );
+            </Form.Item>
+        </Form>
+    );
 };
 
 export default LoginForm;
